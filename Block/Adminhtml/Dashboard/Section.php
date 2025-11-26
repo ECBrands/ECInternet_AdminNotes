9e382d0cb40c5f7f80f84961ef9a9e6b0f565236
Randy Esplin <randy.esplin@gmail.com>
2025-11-26 13:08:39 -0800

    Add strict_types and license

--- Block/Adminhtml/Dashboard/Section.php
+++ Block/Adminhtml/Dashboard/Section.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Copyright (C) EC Brands Corporation - All Rights Reserved
+ * Contact Licensing@ECInternet.com for use guidelines
+ */
+declare(strict_types=1);
 
 namespace ECInternet\AdminNotes\Block\Adminhtml\Dashboard;
 
